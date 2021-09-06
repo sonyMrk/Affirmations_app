d00dc1449db4a633d5d1e531959622ca02b1011a
sonyMrk <murat.kaparov29@gmail.com>
2021-09-06 15:36:22 +0600

    app with recyclerView and cardView

---
+++ app/src/main/java/com/example/affirmations/test.kt
@@ -0,0 +1,8 @@
+package com.example.affirmations
+
+fun main(){
+    val numbers = listOf(0, 2, 3, 4, 5, 1, 2, 9, 6, 8, 7)
+    //println(numbers.sorted())
+    val setOfNumber = numbers.toSet()
+    println(setOfNumber.sorted())
+}
